3cab01dbcf0e6cae9cfc19e9da3c01e0f85b1e29
tredok <wrk.dodge@gmail.com>
2020-04-08 13:22:31 +0300

    step 2

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -2,7 +2,12 @@
 const { program } = require('commander');
 
 program
+  .arguments('<firstConfig>')
+  .arguments('<secondConfig>');
+
+program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information')
   .parse(process.argv);
